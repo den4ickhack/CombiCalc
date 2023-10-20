9e0e8fdfae2410401bd35f427035a2f8857c3131
den4ickhack <45319896+den4ickhack@users.noreply.github.com>
2023-10-20 10:43:06 +0300

    Add files via upload

---
+++ COMBICALC/code/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace CCcode1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.solutionLabel = new System.Windows.Forms.Label();
+            this.kTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.operationComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // nTextBox
+            // 
+            this.nTextBox.Location = new System.Drawing.Point(103, 51);
+            this.nTextBox.Name = "nTextBox";
+            this.nTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nTextBox.TabIndex = 0;
+            this.nTextBox.TextChanged += new System.EventHandler(this.nTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "C(n,k)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(231, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Решить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // solutionLabel
+            // 
+            this.solutionLabel.AutoSize = true;
+            this.solutionLabel.Location = new System.Drawing.Point(312, 135);
+            this.solutionLabel.Name = "solutionLabel";
+            this.solutionLabel.Size = new System.Drawing.Size(59, 13);
+            this.solutionLabel.TabIndex = 3;
+            this.solutionLabel.Text = "Результат";
+            // 
+            // kTextBox
+            // 
+            this.kTextBox.Location = new System.Drawing.Point(103, 86);
+            this.kTextBox.Name = "kTextBox";
+            this.kTextBox.Size = new System.Drawing.Size(100, 20);
+            this.kTextBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "N";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "K";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(315, 86);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(100, 20);
+            this.resultTextBox.TabIndex = 7;
+            // 
+            // operationComboBox
+            // 
+            this.operationComboBox.FormattingEnabled = true;
+            this.operationComboBox.Items.AddRange(new object[] {
+            "Сочетания",
+            "Перестановки",
+            "Разложения"});
+            this.operationComboBox.Location = new System.Drawing.Point(71, 132);
+            this.operationComboBox.Name = "operationComboBox";
+            this.operationComboBox.Size = new System.Drawing.Size(121, 21);
+            this.operationComboBox.TabIndex = 8;
+            this.operationComboBox.SelectedIndexChanged += new System.EventHandler(this.operationComboBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.operationComboBox);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.kTextBox);
+            this.Controls.Add(this.solutionLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label solutionLabel;
+        private System.Windows.Forms.TextBox kTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.ComboBox operationComboBox;
+    }
+}
+
---
+++ COMBICALC/code/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CCcode1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            operationComboBox.SelectedItem = "Сочетания";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(nTextBox.Text, out int n) && int.TryParse(kTextBox.Text, out int k))
+            {
+                string operation = operationComboBox.SelectedItem.ToString();
+                double result = 0;
+                string solution = ""; // Строка для хранения подробного решения
+
+                if (operation == "Сочетания")
+                {
+                    result = CalculateCombinations(n, k);
+                    solution = $"{operation} C({n}, {k}) = {n}! / ({k}! * ({n} - {k})!) = ";
+
+                    for (int i = n; i > n - k; i--)
+                    {
+                        solution += i;
+                        if (i > n - k + 1)
+                        {
+                            solution += " * ";
+                        }
+                    }
+
+                    solution += $" / (";
+
+                    for (int i = k; i >= 1; i--)
+                    {
+                        solution += i;
+                        if (i > 1)
+                        {
+                            solution += " * ";
+                        }
+                    }
+
+                    solution += $") = ({result})";
+                }
+                else if (operation == "Перестановки")
+                {
+                    result = CalculatePermutations(n, k);
+                    solution = $"{operation} P({n}, {k}) = {n}! / ({n} - {k}!) = ";
+
+                    for (int i = n; i >= n - k + 1; i--)
+                    {
+                        solution += i;
+                        if (i > n - k + 1)
+                        {
+                            solution += " * ";
+                        }
+                    }
+
+                    solution += $" = ({result})";
+                }
+                else if (operation == "Разложения")
+                {
+                    result = CalculateArrangements(n, k);
+                    solution = $"{operation} A({n}, {k}) = {n}^{k} = ";
+
+                    for (int i = 1; i <= k; i++)
+                    {
+                        solution += n;
+                        if (i < k)
+                        {
+                            solution += " * ";
+                        }
+                    }
+
+                    solution += $" = ({result})";
+                }
+
+                // Выводим результат и подробное решение
+                solutionLabel.Text = solution;
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные целые числа для n и k.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+
+        private double CalculateCombinations(int n, int k)
+        {
+            if (k < 0 || k > n)
+            {
+                // Обработка некорректных входных данных
+                MessageBox.Show("Значение k должно быть от 0 до n.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0;
+            }
+
+            // Расчет сочетаний C(n, k)
+            double result = Factorial(n) / (Factorial(k) * Factorial(n - k));
+            return result;
+        }
+        private double CalculatePermutations(int n, int k)
+        {
+            if (k < 0 || k > n)
+            {
+                // Обработка некорректных входных данных
+                MessageBox.Show("Значение k должно быть от 0 до n.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0;
+            }
+
+            // Расчет перестановок P(n, k)
+            double result = Factorial(n) / Factorial(n - k);
+            return result;
+        }
+        private double CalculateArrangements(int n, int k)
+        {
+            if (k < 0)
+            {
+                // Обработка некорректных входных данных
+                MessageBox.Show("Значение k должно быть неотрицательным.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0;
+            }
+
+            // Расчет разложений A(n, k)
+            double result = Math.Pow(n, k);
+            return result;
+        }
+        private double Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                double result = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    result *= i;
+                }
+                return result;
+            }
+        }
+
+        private void nTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void operationComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
